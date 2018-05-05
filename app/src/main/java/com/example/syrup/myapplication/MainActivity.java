73f19a4bc9579b4f3ceb31bdd5675941bf43a8bc
SYOM29 <31935019+SYOM29@users.noreply.github.com>
2018-05-05 21:42:17 +0300

    siren

--- app/src/main/java/com/example/syrup/myapplication/MainActivity.java
+++ app/src/main/java/com/example/syrup/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.syrup.myapplication;
 
-import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -9,15 +10,18 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.DialogPreference;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -27,42 +31,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.Object;
-import java.util.UUID;
-
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener
 {
-    //constants
-    private final int REQUEST_CALL = 1;
-
     //variable declaration
+    //foldksljfgsdflgşkdfsgklsfdjhflşkh 19.36
+    //khfkpfkpfkgh
     private ImageView ambulance;
     private ImageView fire;
     private ImageView police;
     private ImageView siren;
     private ImageView SOSButton;
+    private Toast toast;
+    private MediaRecorder mRecorder;
+    private String mFileName = null;
+    private static final String LOG_TAG = "Record_log";
+    private ProgressDialog mProgress;
 
-    private FirebaseStorage storage;
+    private StorageReference storageReference;
 
     //onCreate
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         //setting action bar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setBackground(new ColorDrawable(Color.parseColor("#0c5774")));
-
 
         //setting navigation drawer
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -74,7 +85,6 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-
         //setting the variables
         ambulance = (ImageView)findViewById(R.id.ambulanceButton);
         siren = (ImageView)findViewById(R.id.sirenButton);
@@ -82,38 +92,40 @@ public class MainActivity extends AppCompatActivity
         police = (ImageView)findViewById(R.id.policeButton);
         SOSButton = (ImageView)findViewById(R.id.SOSButton);
 
-        storage = FirebaseStorage.getInstance();
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += "/recorded_audio.3gp";
+        mProgress = new ProgressDialog(this);
+        storageReference = FirebaseStorage.getInstance().getReference();
 
-
-        //setting listeners for widgets
+        //setting listeners
         ambulance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
                 String number = "112";
-                Intent intent = new Intent(Intent.ACTION_CALL);
-                intent.setData(Uri.parse("tel:" + number));
-                //asking permission
-                ActivityCompat.requestPermissions( MainActivity.this, new String[] {android.Manifest.permission.CALL_PHONE}, REQUEST_CALL);
-                if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    return;
-                }
-                startActivity(intent);
+                 Intent intent = new Intent(Intent.ACTION_CALL);
+                 intent.setData(Uri.parse("tel:" + number));
+                 if(ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+                 {
+                return;
+                 }
+                 startActivity(intent);
             }
         });
 
         fire.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
                 String number = "110";
                 Intent intent = new Intent(Intent.ACTION_CALL);
                 intent.setData(Uri.parse("tel:" + number));
-                //asking permission
-                ActivityCompat.requestPermissions( MainActivity.this, new String[] {android.Manifest.permission.CALL_PHONE}, REQUEST_CALL);
-                if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+                {
                     return;
                 }
                 startActivity(intent);
+
             }
         });
 
@@ -124,9 +136,8 @@ public class MainActivity extends AppCompatActivity
                 String number = "155";
                 Intent intent = new Intent(Intent.ACTION_CALL);
                 intent.setData(Uri.parse("tel:" + number));
-                //asking permission
-                ActivityCompat.requestPermissions( MainActivity.this, new String[] {android.Manifest.permission.CALL_PHONE}, REQUEST_CALL);
-                if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                if(ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+                {
                     return;
                 }
                 startActivity(intent);
@@ -142,19 +153,32 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        SOSButton.setOnClickListener(new View.OnClickListener() {
+        SOSButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onTouch(View v, MotionEvent motionEvent)
+            {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN)
+                {
+
+                    startRecording();
+                   toast = Toast.makeText(MainActivity.this, "Recording is started...",Toast.LENGTH_LONG);
+                   toast.show();
+
+
+                }
+                else if(motionEvent.getAction() == MotionEvent.ACTION_UP)
+                {
+                    stopRecording();
+                    toast = Toast.makeText(MainActivity.this, "Recording is stopped...",Toast.LENGTH_LONG);
+                    toast.show();
+
+                }
+                return true;
 
             }
+
+
         });
-
-
-        //setting firebase storage
-        String path = "firememes/" + UUID.randomUUID();
-        //creating base reference
-        StorageReference groupNum = storage.getReference();
-        StorageReference imagesRef = groupNum.child("images");
     }
 
     //onBackPressed
@@ -166,6 +190,49 @@ public class MainActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
+    }
+    private void stopRecording()
+    {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+
+        uploadAudio();
+    }
+    private void uploadAudio()
+    {
+        mProgress.setMessage("Uploading Audio...");
+        mProgress.show();
+
+        StorageReference filepath = storageReference.child("Audio").child("new_audio.mp3");
+        Uri uri = Uri.fromFile(new File(mFileName));
+
+        filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+            {
+                mProgress.dismiss();
+                toast = Toast.makeText(MainActivity.this, "Recording is uploaded succesfully...",Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+    }
+
+    private void startRecording()
+    {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try{
+            mRecorder.prepare();
+        }catch (IOException e)
+        {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+        mRecorder.start();
     }
 
     @Override
@@ -205,7 +272,7 @@ public class MainActivity extends AppCompatActivity
         }
         else if (id == R.id.locations)
         {
-            //Intent goLocations = new Intent(MainActivity.this, LocationsActivity.class);
+            //Intent goLocations = new Intent(MainActivity.this, locations.class);
             //startActivity(goLocations);
         }
         else if (id == R.id.recordings)
@@ -214,8 +281,7 @@ public class MainActivity extends AppCompatActivity
         }
         else if (id == R.id.settings)
         {
-            Intent goSettings = new Intent(MainActivity.this, SettingsActivity.class);
-            startActivity(goSettings);
+
         }
         else if (id == R.id.exit)
         {
