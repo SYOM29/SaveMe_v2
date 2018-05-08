42ad2b22d232de2c721a3be37e782062e5ded023
tolgayan <33233561+tolgayan@users.noreply.github.com>
2018-05-08 18:45:53 +0300

    sdfgn

--- app/src/main/java/com/example/syrup/myapplication/Groups.java
+++ app/src/main/java/com/example/syrup/myapplication/Groups.java
@@ -1,5 +1,6 @@
 package com.example.syrup.myapplication;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,6 +28,7 @@ public class Groups extends AppCompatActivity implements View.OnClickListener {
 
     private EditText groupCodeText;
     private Button enterGroupCode;
+    private Button myGroups;
     private String groupCode;
     private FirebaseFirestore firebaseFirestore;
     private final String TAG = "groupCode";
@@ -38,92 +40,92 @@ public class Groups extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups);
 
-        groupCodeText = findViewById(R.id.groupCode);
-        enterGroupCode = findViewById(R.id.enterCode);
+        groupCodeText  = findViewById(R.id.groupCode );
+        enterGroupCode = findViewById(R.id.enterCode );
+        myGroups       = findViewById(R.id.goToGroups);
 
 
         firebaseFirestore = FirebaseFirestore.getInstance();
         firebaseAuth      = FirebaseAuth.getInstance();
         enterGroupCode.setOnClickListener(this);
+        myGroups.setOnClickListener(this);
 
     }
 
     @Override
-    public void onClick(View view)
-    {
-        groupCode = groupCodeText.getText().toString().trim();
-        String groups = "groups";
+    public void onClick(View view) {
+        if (view == enterGroupCode) {
+            groupCode = groupCodeText.getText().toString().trim();
+            String groups = "groups";
 
-        final DocumentReference docRef = firebaseFirestore.collection(groups).document(groupCode);
+            final DocumentReference docRef = firebaseFirestore.collection(groups).document(groupCode);
 
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>()
-        {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task)
-            {
-                if (task.isSuccessful())
-                {
-                    DocumentSnapshot document = task.getResult();
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
 
-                    if ( document.exists() )
-                    {
-                        String email;
-                        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                        if (document.exists()) {
+                            String email;
+                            FirebaseUser currentUser = firebaseAuth.getCurrentUser();
 
 
+                            Map<String, Object> groupsCollection = new HashMap<String, Object>();
+                            groupsCollection.put(currentUser.getEmail(), currentUser.getUid());
 
-                        Map<String, Object> groupsCollection = new HashMap<String, Object>();
-                        groupsCollection.put( currentUser.getEmail() , currentUser.getUid() );
+                            //add user to places in database
+                            firebaseFirestore.collection("groups").document(groupCode)
+                                    .collection("usersInGroup").document(currentUser.getUid())
+                                    .set(groupsCollection)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Log.d(TAG, "DocumentSnapshot successfully saved!");
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w(TAG, "Error writing document", e);
+                                        }
+                                    });
+                            firebaseFirestore.collection("users").document(currentUser.getUid())
+                                    .collection("groups").document(groupCode)
+                                    .set(groupsCollection)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Log.d(TAG, "DocumentSnapshot successfully saved!");
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w(TAG, "Error writing document", e);
+                                        }
+                                    });
 
-                        //add user to places in database
-                        firebaseFirestore.collection("groups").document( groupCode)
-                                .collection("usersInGroup").document(currentUser.getUid())
-                                .set(groupsCollection)
-                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-                                        Log.d(TAG, "DocumentSnapshot successfully saved!");
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Log.w(TAG, "Error writing document", e);
-                                    }
-                                });
-                        firebaseFirestore.collection("users").document( currentUser.getUid())
-                                .collection("groups").document(groupCode)
-                                .set(groupsCollection)
-                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-                                        Log.d(TAG, "DocumentSnapshot successfully saved!");
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Log.w(TAG, "Error writing document", e);
-                                    }
-                                });
-
-                        Toast.makeText(Groups.this,
-                                "joined successfully", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Groups.this,
+                                    "joined successfully", Toast.LENGTH_SHORT).show();
 
 
+                        } else {
+                            Toast.makeText(Groups.this,
+                                    "Group could not found", Toast.LENGTH_SHORT).show();
+                        }
 
                     }
-
-                    else
-                    {
-                        Toast.makeText(Groups.this,
-                                "Group could not found", Toast.LENGTH_SHORT).show();
-                    }
-
                 }
-            }
 
-        });
+            });
+        }
+
+        else if ( view == myGroups )
+        {
+            Intent goGroups = new Intent(Groups.this, GroupsShowPage.class);
+            startActivity(goGroups);
+        }
     }
 
 
