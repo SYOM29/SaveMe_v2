d0b51912f8d14a0dd0864c502e9e0c4b93eb4742
SYOM29 <31935019+SYOM29@users.noreply.github.com>
2018-05-09 19:35:09 +0300

    j

--- app/src/main/java/com/example/syrup/myapplication/MainActivity.java
+++ app/src/main/java/com/example/syrup/myapplication/MainActivity.java
@@ -378,12 +378,7 @@ return true;
         }
 
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+
     public static void getName()
     {
         mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -396,21 +391,6 @@ return true;
                 }
             }
         });
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
